16e31e42fd49d6871026245d3478cb6e72726bf6
iSi2aka <n.ishizaka@outlook.com>
2023-07-26 16:50:52 +0900

    userモデル バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname,                  presence: true
+  validates :last_name_em,              presence: true
+  validates :first_name_em,             presence: true
+  validates :last_name_kana,            presence: true
+  validates :first_name_kana,           presence: true
+  validates :birth_day,                 presence: true
+  
 end
