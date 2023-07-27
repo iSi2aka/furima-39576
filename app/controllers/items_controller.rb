c70f2bf55e0b0dcc50d1f34026d1516242476a40
iSi2aka <n.ishizaka@outlook.com>
2023-07-28 07:22:35 +0900

    routing と action の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end 
+  end
+
 end
