3b90c9473911e2b3f9a230e1106093721e2ad3e0
iSi2aka <n.ishizaka@outlook.com>
2023-07-27 21:19:32 +0900

    データ型の修正

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_071008) do
     t.string "first_name_em", null: false
     t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.string "birth_day", null: false
+    t.date "birth_day", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
