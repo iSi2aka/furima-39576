ccb1fa6b97b510bf43666da44fc7c1257699a9a9
iSi2aka <n.ishizaka@outlook.com>
2023-07-28 18:30:01 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :days_to_ship
 
+  validates :image,           presence: true
   validates :title,           presence: true
   validates :description,     presence: true 
   validates :category_id,     numericality: { other_than: 1, message: "can't be blank" }
@@ -13,7 +14,8 @@ class Item < ApplicationRecord
   validates :fees_burden_id,  numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id,   numericality: { other_than: 1, message: "can't be blank" }
   validates :days_to_ship_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price,           presence: true
+  validates :price,           presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                              format: { with: /\A[0-9]+\z/, allow_blank: true }
 
   belongs_to :user
   has_one_attached :image
