dfd784f9c340351453ac083329e664f709caa7d7
iSi2aka <n.ishizaka@outlook.com>
2023-07-29 16:08:12 +0900

    任意修正実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = current_user.items.build(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
