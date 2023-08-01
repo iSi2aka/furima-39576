75c634bac6684d72e3f95506f93480ebdb4b01c8
iSi2aka <n.ishizaka@outlook.com>
2023-08-01 12:44:21 +0900

    addresses/ordersテーブル カラム設定

---
+++ db/migrate/20230801020624_create_addresses.rb
@@ -0,0 +1,15 @@
+class CreateAddresses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :addresses do |t|
+      t.string      :postal_code,       null: false
+      t.integer     :prefecture_id,     null: false
+      t.string      :city,              null: false
+      t.string      :street_num,        null: false
+      t.string      :building_num
+      t.string      :phone_num,         null: false
+      t.references  :order,             null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
