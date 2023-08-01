a5a02b16c062387d1053c724c796fdaf806b368c
iSi2aka <n.ishizaka@outlook.com>
2023-08-01 23:10:14 +0900

    商品購入情報 保存の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,19 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :set_order, only: [:index, :create]
 
   def index
-  end
-  
-  def new
     @order_address = OrderAddress.new
   end
-
+  
   def create
+    @user = current_user
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      @order_address.save
+      @order_address.save(@user.id, @item.id)
       redirect_to root_path
     else 
-      render :new, status: :unprocessable_entity
+      render :index, status: :unprocessable_entity
     end
   end
 
@@ -23,6 +22,10 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_address)
           .permit(:postal_code, :prefecture_id, :city, :street_num, :building_num, :phone_num)
-          .merge(user_id: current_user_id)
+          .merge(user_id: current_user.id, item_id: params[:item_id])
   end
+
+  def set_order
+    @item = Item.find(params[:item_id])
+  end 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,21 +1,23 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :street_num, :building_num, :phone_num,
-                :order_id, :user_id, :item_id
+                :user_id, :item_id
 
   with_options presence: true do
-    validates :postal_code,     format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :city,            format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は（全角）で入力する必要があります"	}  
-    validates :street_num,      format: { with: /\A[ぁ-んァ-ヶ一-龥々ー0-9a-zA-Z^\-]+\z/, message: "は ハイフン（-）以外の記号は使用できません" }
-    validates :phone_num,       format: { with: /\A\d{1,11}\z/, message: "は（半角数字）最大11文字で入力する必要があります" }
-    validates :order_id
+    validates :postal_code,     format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)", allow_blank: true}
+    validates :city,            format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は（全角）で入力する必要があります", allow_blank: true	}  
+    validates :street_num,      format: { with: /\A[ぁ-んァ-ヶ一-龥々ー0-9a-zA-Z^\-]+\z/, message: "は ハイフン（-）以外の記号は使用できません", allow_blank: true }
+    validates :phone_num,       format: { with: /\A\d{1,11}\z/, message: "は（半角数字）最大11文字で入力する必要があります", allow_blank: true }
     validates :user_id
     validates :item_id
+   
   end 
   validates :prefecture_id,   numericality: { other_than: 1, message: "can't be blank" }
 
-  def save
+  def save(user_id, item_id)
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street_num: street_num, phone_num: phone_num, order_id: order.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street_num: street_num, building_num: building_num, phone_num: phone_num, order_id: order.id)
   end
 end
+
+ 
